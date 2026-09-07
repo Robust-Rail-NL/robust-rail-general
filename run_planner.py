3bf7183c6732ef3a931725f3a589ded34a65da85
Léon Planken <l.r.planken@tudelft.nl>
2026-09-07 14:56:45 +0200

    Use floating version tags

--- run_planner.py
+++ run_planner.py
@@ -49,11 +49,10 @@ CONTAINER_DB = "/app/database"
 #          departing train never moved — fine on SimpleService, dead on
 #          KleineBinckhorst.
 
-#TODO make 'stable' a floating version and always pull it
 DOCKER_IMAGE_VERSIONS = {
-    "stable": "ghcr.io/robust-rail-nl/planner:0.4.0",
-    "stable-assert": "ghcr.io/robust-rail-nl/planner:0.4.0",
-    "edge": "ghcr.io/robust-rail-nl/planner:0.4.0",
+    "stable": "ghcr.io/robust-rail-nl/planner:latest",
+    "stable-assert": "ghcr.io/robust-rail-nl/planner:latest",
+    "edge": "ghcr.io/robust-rail-nl/planner:latest",
     "local": "planner:latest",
 }
 
